077218e1f730dbb19024d70107ea8fdcff06b3f6
Xiaomai <xsbugh@gmail.com>
2020-04-25 17:42:27 +0800

    fixed link error

--- repo/js-experiment5-ex/index5ex-1-1.js
+++ repo/js-experiment5-ex/index5ex-1-1.js
@@ -1,12 +1,27 @@
 // 随机生成6位数的验证码，包含数字或大小写字母，并将验证码显示在文本框中。
 var code = new Array();
 var textBox = document.getElementById("verifyCode");
+var userInput = document.getElementById("userInput");
+var result = document.getElementById("result");
 
 function RndChar() {
     var str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     return str[Math.floor(Math.random() * str.length)];
 }
-for (let i = 0; i < 6; i++) {
-    code[i] = RndChar();
+
+function GenCode(){
+    for (let i = 0; i < 6; i++) {
+        code[i] = RndChar();
+    }
+    textBox.value = code.join("");
 }
-textBox.value = code.join("");
+
+function Verify(){
+    var userIn = userInput.value.toUpperCase();
+    var test = textBox.value.toUpperCase()
+    if (userIn == test) {
+        result.innerHTML = `验证正确`;
+    } else {
+        result.innerHTML = `验证码错误`;
+    }
+}
